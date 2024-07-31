8dc60c159a9bd2571c62339c42133e7628c1d1a0
daftylooper <pranav.ajaydesai@gmail.com>
2024-07-31 12:20:54 +0530

    add run.bat

---
+++ run.bat
@@ -0,0 +1,5 @@
+cd "e-commerce-frontend"
+START npm start
+cd ..
+cd "e-commerce-server/app"
+uvicorn main:app --reload
--- setup.bat
+++ setup.bat
@@ -1,6 +1,7 @@
-cd e-commerce-frontend
-npm i
+cd "e-commerce-frontend"
+START npm i
 cd ..
-cd e-commerce-server
+cd "e-commerce-server"
 pip install -r requirements.txt
-cd ..
+
+
